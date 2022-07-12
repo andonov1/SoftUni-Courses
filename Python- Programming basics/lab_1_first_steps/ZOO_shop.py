8a897efca20885b476247b618a699b707f3e5a97
Nikolay Andonov <nikolayandonov.p@gmail.com>
2022-07-12 12:01:01 +0300

    Made changes

---
+++ Python- Programming basics/lab_1_first_steps/ZOO_shop.py
@@ -0,0 +1,6 @@
+quantity_dog_food = int(input())
+quantity_cat_food = int(input())
+price_dog_food = quantity_dog_food * 2.50
+price_cat_food = quantity_cat_food * 4
+total = price_cat_food + price_dog_food
+print(f"{total} lv.")
---
+++ Python- Programming basics/lab_1_first_steps/lice na pravougulnik.py
@@ -0,0 +1,4 @@
+a = int(input())
+b = int(input())
+result = b * a
+print(result)
