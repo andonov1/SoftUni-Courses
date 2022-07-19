04bd9c781696849a968da4c10ed4ab9eb75eed54
Nikolay Andonov <nikolayandonov.p@gmail.com>
2022-07-19 22:26:46 +0300

    Updated files.

--- Python- programming basics course/exams/birthday_party.py
+++
@@ -1,6 +0,0 @@
-rent_place = int(input())
-
-cake = rent_place * 0.20
-drinks = cake * 0.55
-animator = rent_place * 1/3
-print(rent_place + cake + drinks + animator)
--- Python- programming basics course/lab_4_for_cycle/Character_Sequence.py
+++
@@ -1,3 +0,0 @@
-text = input()
-for i in range(len(text)):
-    print(i)
