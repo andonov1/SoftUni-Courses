3abcad545ad1c541dc5ed7f931b0755384ea57ef
Nikolay Andonov <nikolayandonov.p@gmail.com>
2022-07-20 14:23:10 +0300

    Upload files.

--- Python-Technology-Fundamentals/Labs/data_types_and_variables/next_happy_year.py
+++
@@ -1,9 +0,0 @@
-year = int(input())
-condition = False
-while not condition:
-    year += 1
-    set_year = set()
-    for i in range(len(str(year))):
-        set_year.add(str(year)[i])
-        condition = len(set_year) == len(str(year))
-print(year)
--- Python-Technology-Fundamentals/Labs/data_types_and_variables/pounds_to_dollars.py
+++
@@ -1,3 +0,0 @@
-pound = float(input())
-dollars = pound * 1.31
-print(f'{dollars:.3f}')
