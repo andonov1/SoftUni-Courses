28e9ff44b134c8401ee750643d566eb1a4924eaa
Nikolay Andonov <nikolayandonov.p@gmail.com>
2022-09-01 22:25:29 +0300

    Added new files.

---
+++ Python-OOP/Exercises/first_steps_in_oop/pokemons.py
@@ -0,0 +1,13 @@
+from Exercises.first_steps_in_oop.project.pokemon import Pokemon
+from Exercises.first_steps_in_oop.project.trainer import Trainer
+
+pokemon = Pokemon("Pikachu", 90)
+print(pokemon.pokemon_details())
+trainer = Trainer("Ash")
+print(trainer.add_pokemon(pokemon))
+second_pokemon = Pokemon("Charizard", 110)
+print(trainer.add_pokemon(second_pokemon))
+print(trainer.add_pokemon(second_pokemon))
+print(trainer.release_pokemon("Pikachu"))
+print(trainer.release_pokemon("Pikachu"))
+print(trainer.trainer_data())
