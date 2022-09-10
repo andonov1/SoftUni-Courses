6f588cedc6187c25358b753494188c3f13106f46
Nikolay Andonov <nikolayandonov.p@gmail.com>
2022-09-10 17:51:09 +0300

    Added new files.

---
+++ Python-OOP/Labs/class_and_static_methods/hotel_rooms/hotel.py
@@ -0,0 +1,31 @@
+class Hotel:
+    def __init__(self, name):
+        self.name = name
+        self.rooms = []
+
+    @property
+    def guests(self):
+        return sum([room.guests for room in self.rooms])
+
+    @classmethod
+    def from_stars(cls, stars_count):
+        return cls(f"{stars_count} stars Hotel")
+
+    def add_room(self, room):
+        self.rooms.append(room)
+
+    def take_room(self, room_number, people):
+        for room in self.rooms:
+            if room.number == room_number:
+                return room.take_room(people)
+
+    def free_room(self, room_number):
+        for room in self.rooms:
+            if room.number == room_number:
+                return room.free_room()
+
+    def status(self):
+        free_rooms = [str(room.number) for room in self.rooms if not room.is_taken]
+        taken_rooms = [str(room.number) for room in self.rooms if room.is_taken]
+        result = f"Hotel {self.name} has {self.guests} total guests\nFree rooms: {', '.join(free_rooms)}\nTaken rooms: {', '.join(taken_rooms)}"
+        return result
