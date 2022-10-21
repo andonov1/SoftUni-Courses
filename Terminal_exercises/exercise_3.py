e6751fd4c0cfcf13cdf5c24827e841abe1cbef9d
andonov1 <nikolayandonov.p@gmail.com>
2022-10-21 16:09:25 +0300

    Deleted folder

--- Terminal_exercises/exercise_3.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-first_word, second_word = sys.argv[1:]
-words_are_anagrams = True
-
-if sorted(first_word.lower()) != sorted(second_word.lower()):
-    words_are_anagrams = False
-
-print(words_are_anagrams, end='')
--- Terminal_exercises/exercise_8.py
+++
@@ -1,8 +0,0 @@
-import sys
-d = {1: 'a', 2: 'b', 3: 'c', 4: 'a', 5: 'd', 6: 'e', 7: 'a', 8: 'b'}
-result = []
-searched_value = sys.argv[1]
-for key, value in d.items():
-    if value == searched_value:
-        result.append(key)
-print(result, end='')
